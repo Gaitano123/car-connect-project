8bebcceac5fcad0dab4fa37eeb2ba2c18b3eeeba
Gaitano <gaitano.george@student.moringaschool.com>
2023-09-05 13:56:41 +0300

    created the Garage, Owner and Car modules

--- app/model.py
+++ app/model.py
@@ -1,4 +1,4 @@
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine, Column, String, Integer, ForeignKey
 
@@ -6,9 +6,41 @@ from sqlalchemy import create_engine, Column, String, Integer, ForeignKey
 engine = create_engine('sqlite:///project.db')
 Base = declarative_base()
 
-
-
-
+class Garage(Base):
+    __tablename__ = 'garages'
+    
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    
+    car = relationship('Car', back_populates='garage')
+    
+    
+    
+class Owner(Base):
+    __tablename__ = 'owners'
+    
+    id= Column(Integer(), primary_key=True)
+    name = Column(String())
+    
+    car = relationship('Car', back_populates='owner')
+    
+    
+    
+class Car(Base):
+    
+    __tablename__ = 'cars'
+    
+    id = Column(Integer(), primary_key=True)
+    make = Column(String())
+    model = Column(String())
+    year = Column(Integer())
+    owner_id = Column(Integer(), ForeignKey('owners.id'))
+    garage_id = Column(Integer(), ForeignKey('garages.id'))
+    
+    garage = relationship("Garage", back_populates='cars')
+    owner = relationship('Owner', back_populates='cars')
+    
+    
 
 
 
